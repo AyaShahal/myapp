b59fb19df5343b13c3e8c3528be6bbc5d9cc75fb
Aya Shahal <ayashahal055@gmail.com>
2023-07-20 10:36:24 +0300

    bottom navigation

---
+++ lib/bottomnavigation.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import './main.dart';
+import './search.dart';
+import './profile.dart';
+
+class MyWidget extends StatefulWidget {
+  MyWidget({Key? key}) : super(key: key);
+
+  @override
+  _MyWidgetState createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    // Navigation logic
+    switch (index) {
+      case 0:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => MyApp()),
+        );
+        break;
+      case 1:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Search()),
+        );
+        break;
+      case 2:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Profile()),
+        );
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      selectedIconTheme: IconThemeData(color: Colors.black87),
+      unselectedIconTheme: IconThemeData(color: Colors.grey.shade500),
+      type: BottomNavigationBarType.fixed,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      elevation: 0,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home, color: Colors.black87),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search, color: Colors.black87),
+          label: 'Search',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person, color: Colors.black87),
+          label: 'Profile',
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+    );
+  }
+}
